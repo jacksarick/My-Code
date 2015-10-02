1f1a3f40d61c9115c3c0d437525a163878c84bb9
Jackie <jacksarick@gmail.com>
2015-10-02 14:02:14 -0400

    PDF converting program

---
+++ Python/fuck if i know/PDF.py
@@ -0,0 +1,8 @@
+dic1 = ["'a", "3b", ".c", "(d", "#e", "Bf", "Cg", '"h', "1i", "jj", "4k", "Al", "-m", "?n", "&o", "Fp", "Mq", ",r", "2s", "0t", "Eu", "Iv", "@w", "xx", "*y", "zz"]
+dic1 = [list(n) for n in dic1]
+
+dic = [dic1]
+
+for dictionary in dic:
+	for pair in dictionary:
+		
