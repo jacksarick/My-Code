74fe78fbdb836908acc85082d05e60bbfd183170
Jack Sarick <jacksarick@gmail.com>
2017-01-08 00:02:05 -0700

    Well I broke everything, but now it's fixed

--- Racket/triple_helix.rkt
+++ Racket/triple_helix.rkt
@@ -5,18 +5,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;Macro for 'defn'
-;; (define (λ () ...)) -> (defn () ...)
+;;macro (define (λ () ...)) -> (defn () ...)
 (define-syntax-rule (defn name args body...)
   (define name
     (λ args
       body...)))
+
+;Macro for renaming function
+;;macro (defn x a (y a)) -> (alt y x)
+(define-syntax-rule (alt old new)
+	(defn new args (apply old args)))
 
 ;For (x y) returns ((+ x y) y)
 ;; (value increment) -> (new-value increment)
 (defn update (x)
   (list (+ (car x) (cadr x)) (cadr x)))
 
-;Function for degrees -> radians
+;Function for degrees to radians
 ;; degrees -> radians
 (defn rad (x)
   (* x (/ pi 180)))
@@ -25,6 +30,9 @@
 ;; point, distance from centre, function, rotation -> new point
 (defn transform (p d f r)
   (+ p (* d (f (rad r)))))
+
+;;alt transform -> tform
+(alt transform tform)
 
 ;Function to draw a circle with centre at (x y)
 ;; context, x coord , y coord, radius -> nil
@@ -36,7 +44,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 
 ;Draw our character
-;; context, x coord, y coord, distance of circle from centre, rotation of charachter -> nil
+;; context, x coord, y coord, radius, rotation -> nil
 (defn draw-char (dc x y d r)
   ;Define a radius
   (let ([radius 30])
@@ -44,9 +52,9 @@
     ;Draw the char at center (x, y)
     (send dc set-pen   "black" 1 'solid)
     (send dc set-brush "black"   'solid)
-    (draw-circle dc (transform x d sin (+ r   0)) (transform y d cos (+ r   0)) radius)
-    (draw-circle dc (transform x d sin (+ r 120)) (transform y d cos (+ r 120)) radius)
-    (draw-circle dc (transform x d sin (+ r 240)) (transform y d cos (+ r 240)) radius)))
+    (draw-circle dc (tform x d sin (+ r   0)) (tform y d cos (+ r   0)) radius)
+    (draw-circle dc (tform x d sin (+ r 120)) (tform y d cos (+ r 120)) radius)
+    (draw-circle dc (tform x d sin (+ r 240)) (tform y d cos (+ r 240)) radius)))
 
 ;Function triggered on key stroke
 ;; racket-event -> nil
@@ -67,11 +75,12 @@
 ;Function that is run every tick
 ;; canvas, context -> nil
 (defn game-tick (canvas dc)
-  ;Clear the board
-  (send dc erase)
-
-  ;Draw the character
-  (draw-char dc (car char-x) (car char-y) (car char-d) (car char-r)))
+  (begin
+    ;Clear the board
+    (send dc erase)
+  
+    ;Draw the character
+    (draw-char dc (car char-x) (car char-y) (car char-d) (car char-r))))
 
 ;Define our character's stats
 (define char-x '(250 0))
@@ -100,14 +109,15 @@
 
 ;Define our game loop to update 60 times per second
 (defn loop ()
-  ;Update our character
-  (set! char-x (update char-x))
-  (set! char-y (update char-y))
-  (set! char-r (list (+ (car char-r) (cadr char-r)) (cadr char-r)))
-
-  (send canvas on-paint)
-  (sleep/yield (/ 1 60))
-  (loop))
+  (begin
+    ;Update our character
+    (set! char-x (update char-x))
+    (set! char-y (update char-y))
+    (set! char-r (list (+ (car char-r) (cadr char-r)) (cadr char-r)))
+    
+    (send canvas on-paint)
+    (sleep/yield (/ 1 60))
+    (loop)))
 
 
 ;;;;;;;;;;;;
